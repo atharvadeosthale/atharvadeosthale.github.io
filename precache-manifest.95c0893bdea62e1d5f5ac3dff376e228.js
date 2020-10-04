ff2be2adfb24d96614592acc69431d023d6fa3e1
Atharva Deosthale <atharva.deosthale17@gmail.com>
2020-10-05 03:44:18 +0530

    Website Revamp

---
+++ precache-manifest.95c0893bdea62e1d5f5ac3dff376e228.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309f6962fdc7ec7d2641e38c897baae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45dcfdae36188f86b637",
+    "url": "/static/css/2.b6fc6fc7.chunk.css"
+  },
+  {
+    "revision": "31b140b3d1957c486898",
+    "url": "/static/css/main.eddbc5e6.chunk.css"
+  },
+  {
+    "revision": "45dcfdae36188f86b637",
+    "url": "/static/js/2.5045b2b7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5045b2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b140b3d1957c486898",
+    "url": "/static/js/main.295c2d5f.chunk.js"
+  },
+  {
+    "revision": "3b2bda409d83d3f997fa",
+    "url": "/static/js/runtime-main.2fc13a78.js"
+  },
+  {
+    "revision": "a5530e12211a5b446a1812f14b9d75e0",
+    "url": "/static/media/header_bg.a5530e12.jpg"
+  }
+]);
